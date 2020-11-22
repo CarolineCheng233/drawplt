edc036d4633f59fa94f0338b6e79f42d32a9c776
CarolinaCheng616 <948005247@qq.com>
2020-11-22 15:36:44 +0800

    polish code

--- draw_bar.py
+++ draw_bar.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def _figure_bar(figsize, fontsize, ticksize, xlabel, ylabel, x, y, xticks, log=False):
+def _figure_bar(fontsize, ticksize, ylabel, x, y, xticks, log=False):
     # plt.style.use('ggplot')
     fig = plt.figure()
     ax = plt.subplot(111)
-    font = {'family': 'Microsoft JhengHei', 'weight': 'bold', 'size': fontsize, }
+    # font = {'family': 'Microsoft JhengHei', 'weight': 'bold', 'size': fontsize}  # noqa: E501
     # plt.rc('font', **font)
     # plt.rc('axes', axisbelow=False)
 
@@ -16,12 +17,15 @@ def _figure_bar(figsize, fontsize, ticksize, xlabel, ylabel, x, y, xticks, log=F
     plt.bar(x=x, height=y, width=0.6, log=log, zorder=10)
 
     plt.ylabel(ylabel, fontsize=fontsize, fontweight='bold')
-    plt.xticks(np.arange(len(xticks)), xticks, fontsize=ticksize, fontweight='bold')
+    plt.xticks(np.arange(len(xticks)),
+               xticks,
+               fontsize=ticksize,
+               fontweight='bold')
     plt.yticks(fontsize=ticksize, fontweight='bold')
     # 不显示刻度线
     ax.xaxis.set_ticks_position('none')
     ax.yaxis.set_ticks_position('none')
-    ax.set_xlim(-1,)
+    ax.set_xlim(-1, )
     # 显示横向的网格
     plt.grid(axis='y', zorder=1)
     # 标签尾与刻度对齐
