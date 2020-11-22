30922df952c84f0c04918f9b78e927d76f46eded
chy <948005247@qq.com>
2020-11-22 13:56:25 +0800

    init commit

---
+++ draw_bar.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+
+def _figure_bar(figsize, fontsize, ticksize, xlabel, ylabel, x, y, xticks, log=False):
+    # plt.style.use('ggplot')
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    font = {'family': 'Microsoft JhengHei', 'weight': 'bold', 'size': fontsize, }
+    # plt.rc('font', **font)
+    # plt.rc('axes', axisbelow=False)
+
+    # 设置右边和上边的框不显示
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    plt.bar(x=x, height=y, width=0.6, log=log, zorder=10)
+
+    plt.ylabel(ylabel, fontsize=fontsize, fontweight='bold')
+    plt.xticks(np.arange(len(xticks)), xticks, fontsize=ticksize, fontweight='bold')
+    plt.yticks(fontsize=ticksize, fontweight='bold')
+    # 不显示刻度线
+    ax.xaxis.set_ticks_position('none')
+    ax.yaxis.set_ticks_position('none')
+    ax.set_xlim(-1,)
+    # 显示横向的网格
+    plt.grid(axis='y', zorder=1)
+    # 标签尾与刻度对齐
+    fig.autofmt_xdate(rotation=45)
+    fig.tight_layout()
+    plt.show()
